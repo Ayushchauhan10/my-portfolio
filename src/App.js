e9622369f6a085959618a91039aa530a0e1fd719
Ayush Chauhan <chauhan.ayush.b2@gmail.com>
2024-08-11 09:14:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,19 +8,27 @@ import Skills from './components/Skills';
 import { Element } from 'react-scroll';
 
 const App = () => {
+  // Retrieve the current theme ('light' or 'dark') from the GlobalContext
   const { theme } = useContext(GlobalContext);
 
-
   return (
+    // Wrapper to control overflow of content
     <div className='overflow-hidden'>
+      {/* Navbar component, usually containing site navigation links */}
       <Navabar />
+      
+      {/* Main content area, with dynamic background and text color based on the theme */}
       <div className={`min-h-screen ${theme === 'light' ? 'text-greyText bg-lightBg' : 'text-lightBg bg-darkBg'}`}>
+        {/* Wrapping components with 'Element' to enable smooth scrolling to these sections */}
         <Element name="home"><Home /></Element>
         <Element name="projects"><Projects /></Element>
         <Element name="skills"><Skills /></Element>
         <Element name="contact"><Contact /></Element>
       </div>
+      
+      {/* Footer section with dynamic background color, aligned center */}
       <footer className={`w-full text-center text-white font-bold h-[60px] bg-${theme === 'light' ? 'lightTheme' : 'darkTheme'} flex items-center justify-center`}>
+        {/* Footer content */}
         <div>Made with ❤️ by Ayush Chauhan. <br />All rights reserved</div>
       </footer>
     </div>
